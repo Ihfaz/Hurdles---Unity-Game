e1b005899cabbe2fa12585b38609c15be723e2f1
Ihfaz <ihfaz.98@gmail.com>
2020-10-30 19:46:27 -0500

    Player movement refined

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 200f;
+    public float sidewaysForce = 100f;
+
+    void FixedUpdate()
+    {
+        rb.AddForce (0, 0, forwardForce * Time.deltaTime);
+
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			rb.AddForce (sidewaysForce * Time.deltaTime, 0, 0);
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			rb.AddForce (-sidewaysForce * Time.deltaTime, 0, 0);
+		}
+
+		// if (rb.position.y < -1f) {
+		// 	FindObjectOfType<GameManager> ().EndGame ();
+		// }
+    }
+}
